a99375f86bd5808815b597add99c5ac62cc7ef42
ThinhKhanh <thinhkhanh07@gmail.com>
2025-07-09 00:03:23 +0700

    sửa lại phần router products

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,11 +1,11 @@
 import expree from "express"
-import { getAllProducts } from "../controllers/products.controller";
+import { getAllProducts, getProductById, productCreate, productDelete, productUpdate } from "../controllers/products.controller";
 
 const productRouter = expree.Router();
 productRouter.get("/", getAllProducts);
-productRouter.get("/:id", getAllProducts);
-productRouter.post("/", getAllProducts);
-productRouter.put("/:id", getAllProducts);
-productRouter.delete("/id", getAllProducts);
+productRouter.get("/:id", getProductById);
+productRouter.post("/", productCreate);
+productRouter.put("/:id", productUpdate);
+productRouter.delete("/id", productDelete);
 
 export default productRouter;
