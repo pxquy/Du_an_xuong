9b1498db2e30c7b638cd7d931d5ba7281fcad3f9
ThinhKhanh <thinhkhanh07@gmail.com>
2025-07-24 11:30:27 +0700

    khánh cấp nhật lại

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -5,7 +5,10 @@ export const getAllCategories = async (req,res) => {
         const categories = await Category.find();
         res.status(201).json(categories)
     } catch (error) {
-        res.status(500).json(error,"Lỗi")
+       res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 
@@ -15,24 +18,33 @@ export const getCategoriesById = async (req,res) => {
         if(!category) return res.status(404).json({message:"Danh mục không tồn tại"});
         res.status(201).json(category)
     } catch (error) {
-         res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 export const categoriesCreate = async (req,res) => {
     try {
         const category = await Category.create(req.body);
-        res.status(200).json({message:"Thêm thành công"}, category);
+        res.status(200).json({message:"Thêm thành công", category});
     } catch (error) {
-         res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 export const categoriesUpdate = async (req,res) => {
     try {
         const category = await Category.findByIdAndUpdate(req.params.id, rea.body, {new: true});
         if(!category) return res.status(404).json({message:"Danh mục không tồn tại"});
-        res.status(201).json({message:"cập nhật thành công"},category)
+        res.status(201).json({message:"cập nhật thành công",category})
     } catch (error) {
-         res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 export const categoriesDelete = async (req,res) => {
@@ -41,6 +53,9 @@ export const categoriesDelete = async (req,res) => {
         if(!category) return res.status(404).json({message:"Danh mục không tồn tại"});
         res.status(201).json({message:"Xoá thành công"})
     } catch (error) {
-         res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,7 +5,10 @@ export const getAllProducts = async (req,res) => {
         const products = await Product.find().populate("category_id");
         res.status(201).json(products)
     } catch (error) {
-        res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 
@@ -15,24 +18,33 @@ export const getProductById = async (req,res) => {
         if (!product) return res.status(404).json({message: "Sản phẩm không tồn tại"});
         res.status(201).json(product)
     } catch (error) {
-        res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 export const productCreate = async (req,res) => {
     try {
         const product = await Product.create(req.body)
-        res.status(200).json({message:"Thêm thành công"}, product)
+        res.status(200).json({message:"Thêm thành công",product})
     } catch (error) {
-        res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 export const productUpdate = async (req,res) => {
     try {
         const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
         if (!product) return res.status(404).json({message: "Sản phẩm không tồn tại"});
-        res.status(201).json({message:"Cập nhật thành công"},product)
+        res.status(201).json({message:"Cập nhật thành công",product})
     } catch (error) {
-        res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
 export const productDelete = async (req,res) => {
@@ -41,6 +53,9 @@ export const productDelete = async (req,res) => {
         if (!product) return res.status(404).json({message: "Sản phẩm không tồn tại"});
         res.status(201).json({message:"Xoá thành công"})
     } catch (error) {
-        res.status(500).json(error,"Lỗi")
+        res.status(500).json({
+            message: "Lỗi",
+            error: error.message
+        });
     }
 }
